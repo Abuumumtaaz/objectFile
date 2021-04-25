26f5a39de12147f8e2b7d3610b9ae0872f187d31
unknown <ahmedqayr22@gmail.com>
2021-04-25 19:29:24 +0100

    add new

--- object.js
+++ object.js
@@ -1,9 +1,11 @@
-class User  {
+  /*
+  class User{
     constructor(username, email){
         this.username = username;
         this.email = email;
         this.score = 0;
     }
+  
     login(){
         console.log(`${this.username} just logged in`);
         return this;
@@ -12,12 +14,15 @@ class User  {
         console.log(`${this.username} just logged out`);
         return this;
     }
+    
     inScore(){
         this.score += 1;
         console.log(`${this.username} his score is ${this.score}`);
         return this;
     }
 }
+*/
+
 //subclass 
 class Admin extends User{
     constructor(username, email, title){
@@ -28,15 +33,27 @@ class Admin extends User{
 }
 }
 //constractor function
+
 function User(username, email){
     this.username = username;
     this.email = email;
-    this,login = function(){
+    this.score = 0;
+    
+} 
+//use prototype 
+User.prototype.login = function() {
+    console.log(`${this.username} just logged in`);
+    return this;
+};
+User.prototype.logout = function() {
+    console.log(`${this.username} just logged out`);
+};
+User.prototype.inScore = function() {
+        this.score += 1;
         console.log(`${this.username} his score is ${this.score}`);
-        
-    };
+        return this;
+};
 
-}
 const userOne = new User('Ahmed', 'ahmedqayr22@gmail.com');
 const userTwo = new User('Ramla', 'rayelmi22@gmail.com');
 const userThree = new User('Damla', 'elmi22@gmail.com');
@@ -45,11 +62,15 @@ const userFive = new Admin('Raage', 'raage22@gmail.com', 'pro');
 
 console.log(userFive, userOne, userFour, userTwo);
 userOne.login(); 
-userOne.inScore();
+userTwo.login();
+userTwo.logout();
 userOne.logout();
-userTwo.login().logout().inScore();
-userFour.inScore().inScore();
-userThree.login().login().login().logout();
+userOne.inScore();
+userTwo.inScore();
+console.log(users);
+//userTwo.login().logout().inScore();
+//userFour.inScore().inScore();
+//userThree.login().login().login().logout();
 
 let users = [ userOne, userTwo, userThree, userFour, userFive,];
 console.log(users);
@@ -57,3 +78,6 @@ console.log(users);
 userFive.deleteUser(userThree);
 userFive.deleteUser(userFour);
 console.log(users);
+
+
+
