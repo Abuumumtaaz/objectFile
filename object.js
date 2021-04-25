b61c15fdc922f6fb6c50c45db9832757b4afc987
unknown <ahmedqayr22@gmail.com>
2021-04-25 17:51:42 +0100

    add new

--- object.js
+++ object.js
@@ -18,17 +18,30 @@ class User  {
         return this;
     }
 }
-
+//subclass 
 class Admin extends User{
-    deleteUser(){
-
+    constructor(username, email, title){
+        super(username, email);
+        this.title = title;
     }
+    deleteUser(user){ users = users.filter(u => u.username !== user.username );
+}
+}
+//constractor function
+function User(username, email){
+    this.username = username;
+    this.email = email;
+    this,login = function(){
+        console.log(`${this.username} his score is ${this.score}`);
+        
+    };
+
 }
 const userOne = new User('Ahmed', 'ahmedqayr22@gmail.com');
 const userTwo = new User('Ramla', 'rayelmi22@gmail.com');
 const userThree = new User('Damla', 'elmi22@gmail.com');
 const userFour = new User('Rayaan', 'rayaan22@gmail.com');
-const userFive = new Admin('Raage', 'raage22@gmail.com');
+const userFive = new Admin('Raage', 'raage22@gmail.com', 'pro');
 
 console.log(userFive, userOne, userFour, userTwo);
 userOne.login(); 
@@ -38,6 +51,9 @@ userTwo.login().logout().inScore();
 userFour.inScore().inScore();
 userThree.login().login().login().logout();
 
-let users = [userFive, userOne, userFour, userTwo];
+let users = [ userOne, userTwo, userThree, userFour, userFive,];
 console.log(users);
 
+userFive.deleteUser(userThree);
+userFive.deleteUser(userFour);
+console.log(users);
